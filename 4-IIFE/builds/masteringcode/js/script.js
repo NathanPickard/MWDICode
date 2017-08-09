80635af1a39f54653c8135e8fe2c8b14f03f96c5
Nathan Pickard <nathan.pickard87@gmail.com>
2017-08-08 19:55:08 -0700

    IIFE MC

---
+++ 4-IIFE/builds/masteringcode/js/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var colors = ['#C94C24', //orange
+'#268BD2', //blue
+'#C4226F', //pink
+'#859835', // lime
+'#6D73C2', // purple
+'#37A198', // green
+'#DA3637', //red
+'#F0AD4E' //yellow
+];
+
+function makeBoxes(howMany) {
+  var colorAmt = colors.length;
+  var currColor = 0;
+  var myElement;
+  var myNode = document.querySelector('.boxes');
+
+  for (var i = 0; i < howMany; i++) {
+    myElement = document.createElement('div');
+    myElement.class = 'box';
+    myElement.style = 'background-color: ' + colors[currColor];
+    myNode.appendChild(myElement);
+
+    if (currColor === colorAmt - 1) {
+      currColor = 0;
+    } else {
+      currColor++;
+    }
+  }
+
+  myNode.addEventListener('click', function (e) {
+    e.target.parentNode.removeChild(e.target);
+  }, false);
+}
+
+makeBoxes(10);
+//# sourceMappingURL=script.js.map
