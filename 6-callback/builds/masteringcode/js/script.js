7b29d445084df2c9feceaff933192651052b8f23
Nathan Pickard <nathan.pickard87@gmail.com>
2017-10-05 21:54:54 -0700

    addEventListener for click event callback

--- 6-callback/builds/masteringcode/js/script.js
+++ 6-callback/builds/masteringcode/js/script.js
@@ -1,2 +1,8 @@
+var tips = document.querySelectorAll('.hastip');
 
+for (var i = 0; i < tips.length; i++) {
+  tips[i].addEventListener('click', function (e) {
+    e.target.querySelector('.tooltip').classList.toggle('active');
+  }, false);
+}
 //# sourceMappingURL=script.js.map
