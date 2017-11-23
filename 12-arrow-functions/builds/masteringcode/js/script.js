15516a971ac88f2eb88c8e9857f1152330c43bc1
Nathan Pickard <nathan.pickard87@gmail.com>
2017-11-22 23:04:08 -0800

    Arrow function scaffolding

---
+++ 12-arrow-functions/builds/masteringcode/js/script.js
@@ -0,0 +1,22 @@
+function Hamburger(nodeName) {
+  var myNode = document.querySelector(nodeName + ' .hamburger');
+
+  return {
+    activate: function activate() {
+      myNode.addEventListener('click', function (e) {
+        myNode.parentNode.querySelector('.navbar').classList.toggle('hidden');
+      }, false);
+    },
+    hide: function hide() {
+      myNode.parentNode.querySelector('.navbar').classList.add('hidden');
+    }
+  };
+}
+
+var topMenu = new Hamburger('#topMenu');
+topMenu.activate();
+
+var bottomMenu = new Hamburger('#bottomMenu');
+bottomMenu.activate();
+bottomMenu.hide();
+//# sourceMappingURL=script.js.map
---
+++ 12-arrow-functions/process/js/script.js
@@ -0,0 +1,21 @@
+function Hamburger(nodeName) {
+  var myNode = document.querySelector(nodeName + ' .hamburger');
+
+  return {
+    activate: function() {
+      myNode.addEventListener('click', function(e) {
+      myNode.parentNode.querySelector('.navbar').classList.toggle('hidden');
+      }, false);
+    },
+    hide: function() {
+      myNode.parentNode.querySelector('.navbar').classList.add('hidden');
+    } 
+  } 
+}
+
+var topMenu = new Hamburger('#topMenu');
+topMenu.activate();
+
+var bottomMenu = new Hamburger('#bottomMenu');
+bottomMenu.activate();
+bottomMenu.hide();
